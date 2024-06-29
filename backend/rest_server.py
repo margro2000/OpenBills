9040707d65c5e8fe83333d9b9bb2b1e7a18dc131
Archishman Sravankumar <archishman.sravankumar@gmail.com>
2024-06-29 15:19:29 -0700

    upload file and read it into string

--- backend/rest_server.py
+++ backend/rest_server.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
@@ -6,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/uploadfile/", response_class=PlainTextResponse)
+async def create_upload_file(file: UploadFile = File(...)):
+    content = await file.read()
+    return content.decode("utf-8")
