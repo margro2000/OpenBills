96a6ee29b893458fa174b6ff87172ec08b1ad69c
natasharao <rao117@purdue.edu>
2024-06-29 15:31:48 -0700

    Added bill reader page

---
+++ frontend/pages/billreader.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import { Layout } from "../components/navbar/layout";
+
+const BillReader: NextPage = () => {
+    return (
+       <Layout>
+          
+       </Layout>
+    );
+ };
+ 
+ export default BillReader;
