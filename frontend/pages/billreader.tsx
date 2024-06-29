0a078f771300771e60fbf25dc26a95e95c0f53a0
natasharao <rao117@purdue.edu>
2024-06-29 16:13:07 -0700

    started structure for bill reader page

--- frontend/pages/billreader.tsx
+++ frontend/pages/billreader.tsx
@@ -1,11 +1,30 @@
 import { NextPage } from "next";
 import { Layout } from "../components/navbar/layout";
+import { Button, Card, Grid, Input } from "@nextui-org/react";
 
 const BillReader: NextPage = () => {
   return (
-    <Layout>
-      <div>heyyooo</div>
-    </Layout>
+    <Grid.Container gap={2} justify="center">
+        <Grid xs={6} sm={6}>
+            <Input placeholder="Ask a Question"></Input>
+            <Button>Submit</Button>
+        </Grid>
+        <Grid xs={12} sm={6}>
+            <Card>
+                <Card.Header>Summary/Abstract</Card.Header>
+            </Card>
+        </Grid>
+        <Grid xs={12} sm={6}>
+            <Card>
+                <Card.Header>Recommended Questions</Card.Header>
+            </Card>
+        </Grid>
+        <Grid xs={12} sm={6}>
+            <Card>
+                <Card.Header>Table of Contents/Outline</Card.Header>
+            </Card>
+        </Grid>
+    </Grid.Container>
   );
 };
 
