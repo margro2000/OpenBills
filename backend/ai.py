f0d3f65cefa990063e402bb69cc77faa73e975e0
Archishman Sravankumar <archishman.sravankumar@gmail.com>
2024-06-29 19:18:13 -0700

    gemini merge

--- backend/ai.py
+++ backend/ai.py
@@ -1,41 +1,44 @@
 import os
-from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
-from yap import YAP
+import re
+from tqdm import tqdm
+import google.generativeai as genai
+import hashlib
 
-def deyappify(text, model = "claude-3-5-sonnet-20240620", testing=True):
-    if testing:
-        return text[:500]
-    client = Anthropic()
-    
-    
+def cached_request(prompt, model_name="gemini-1.5-flash", cache_dir='/tmp'):
+    ## import hashlib and get determinstic hash of prompt as prompt_id
+    prompt_id = hashlib.md5(prompt.encode()).hexdigest()
+    print(prompt[:40], prompt_id)
+    if os.path.exists(f'{cache_dir}/{prompt_id}.txt'):
+        with open(f'{cache_dir}/{prompt_id}.txt', 'r') as f:
+            return f.read()
+    genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+    generation_config = {
+            "temperature": 1,
+            "top_p": 0.95,
+            "top_k": 64,
+            "max_output_tokens": 1024,
+            "response_mime_type": "text/plain",
+        }
+    model = genai.GenerativeModel(
+        model_name=model_name,
+        generation_config=generation_config,
+    )
 
-    chunks = [
-        text.split('[[Page 124 STAT. 512]]')
-    ]
-    summaries = []
-    for i, chunk in enumerate(chunks):
-        prompt = f"Please summarize the following chunk of a larger document. This is chunk {i} of {len(chunks)}\n\n{chunk}."
-        chunk_id = hash(chunk)
-        # if chunk_id file exists, read it and append to summaries
-        if os.path.exists(f'/tmp/{chunk_id}.txt'):
-            with open(f'/tmp/{chunk_id}.txt', 'r') as f:
-                summaries.append(f.read())
-            continue
-        message = client.messages.create(
-            model=model,
-            max_tokens=1024,
-            messages=[
-                {"role": "user", "content": prompt}
-            ]
-        )
-        with open(f'/tmp/{chunk_id}.txt', 'w') as f:
-            f.write(prompt)
-        summaries.append(message.content[0].text)
-    
-    return "\n\n".join(summaries)
+    chat_session = model.start_chat(history=[])
+
+    response = chat_session.send_message(prompt)
+    with open(f'{cache_dir}/{prompt_id}.txt', 'w') as f:
+        f.write(response.text)
+    return response.text
+def get_summary_prompt(text, summary_description="100 word"):
+    return f"Create a {summary_description} summary of the following document: {text}"
+def deyappify(text, prompt, model_name="gemini-1.5-flash", generation_config=None):
+    prompt = get_summary_prompt(text, summary_description="100 word")
+    summary = cached_request(prompt, model_name=model_name)
+    return summary
 
 # Example usage
 if __name__ == "__main__":
-    sample_text = YAP
+    sample_text = "INSERT_LONG_DOCUMENT_TEXT_HERE"
     summary = deyappify(sample_text)
     print(summary)
--- backend/rest_server.py
+++ backend/rest_server.py
@@ -3,7 +3,10 @@ from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
 import ai
 from fastapi.middleware.cors import CORSMiddleware
-import json
+
+
+import json 
+TEST = 1
 app = FastAPI()
 origins = [
     "http://localhost:3000",
@@ -26,15 +29,24 @@ class BillSummaryResponse(BaseModel):
 async def create_upload_file(file: UploadFile = File(...)):
     content = await file.read()
     bill_txt = content.decode("utf-8")
+    outliers_prompt = f"""
+    The following is a bill from Congress. Please identify any sections (there may be none) that are addressing 
+    topics that are not directly related to the main purpose of the bill. In particular look for section indicative
+    of pork-barrel spending, unrelated amendments, or other topics that are not directly related to the main purpose
+    of the bill.
+
+    {bill_txt}
+    """
+    if TEST == 1:
+        response = BillSummaryResponse(
+            abstract=bill_txt[:500],
+            summary=bill_txt[:2000],
+            outliers="No outliers",
+        )
+        return response
     response = BillSummaryResponse(
-        abstract=ai.deyappify(bill_txt),
-        summary=bill_txt[:100] + "...",
-        outliers="These are the outliers in the bill"
+        abstract=ai.cached_request(ai.get_summary_prompt(bill_txt, summary_description="100 word")),
+        summary=ai.cached_request(ai.get_summary_prompt(bill_txt, summary_description="bullet pointed (1 for each section of the bill), 500 word")),
+        outliers=ai.cached_request(outliers_prompt),
     )
     return response
-
-
-
-
-
-
