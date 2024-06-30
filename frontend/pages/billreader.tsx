9badad060980f0525dd64b9d5647dc0cab1af34a
natasharao <rao117@purdue.edu>
2024-06-29 17:08:29 -0700

    remove commented code

--- frontend/pages/billreader.tsx
+++ frontend/pages/billreader.tsx
@@ -39,41 +39,4 @@ const BillReader: NextPage = () => {
   );
 };
 
-// const BillReader: NextPage = () => {
-//   return (
-//     <Layout>
-//       <Grid.Container gap={2} css={{ height: '100vh', padding: '1rem' }}>
-//         <Grid xs={12} sm={6} css={{ display: 'flex', flexDirection: 'column',height:'100%', gap: '10px' }}>
-//           <Grid.Container gap={1} alignItems="center">
-//             <Grid xs={8}>
-//               <Input
-//                 placeholder="Ask a Question"
-//                 css={{ width: '100%' }}
-//               />
-//             </Grid>
-//             <Grid xs={4}>
-//               <Button auto color="gradient" ghost>Submit</Button>
-//             </Grid>
-//           </Grid.Container>
-//         </Grid>
-        
-//         <Grid xs={12} sm={6} css={{ height: '50%' }}>
-//           <OutlineCard/>
-//         </Grid>
-
-//         <Grid xs={12} sm={6} css={{ height: '50%' }}>
-//           <AbstractCard/>
-//         </Grid>
-// {/* 
-//         <Grid xs={12} sm={6} css={{ height: '50%' }}>
-//           <Card css={{ height: '100%' }}>
-//             <Card.Header>Recommended Questions/Answers</Card.Header>
-//           </Card>
-//         </Grid> */}
-        
-//       </Grid.Container>
-//     </Layout>
-//   );
-// };
-
 export default BillReader;
